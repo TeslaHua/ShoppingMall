19e7ef5e48873d9f2ca9bfde19710686146c999e
TeslaHua <wangzhonghua_cnu@163.com>
2020-11-14 23:38:54 +0800

    配置和测试Nacos

--- shoppingmall-member/src/main/java/com/teslahua/shoppingmall/member/controller/MemberController.java
+++ shoppingmall-member/src/main/java/com/teslahua/shoppingmall/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
+import com.teslahua.shoppingmall.member.feign.CouponFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,18 @@ import com.teslahua.common.utils.R;
 public class MemberController {
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    CouponFeignService couponFeignService;
+
+    @RequestMapping("/coupons")
+    public R test(){
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setNickname("张三");
+
+        R membercoupons = couponFeignService.membercoupons();
+        return R.ok().put("member",memberEntity).put("coupons",membercoupons.get("coupons"));
+    }
 
     /**
      * 列表
