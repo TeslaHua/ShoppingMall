84e8d1abc5674a771cd4f2c3e516e0883a9a450a
TeslaHua <wangzhonghua_cnu@163.com>
2021-02-21 18:03:03 +0800

    CompletableFuture 计算回调和感知

--- shoppingmall-search/src/main/java/com/teslahua/shoppingmall/search/Thread/completableFutureTest.java
+++ shoppingmall-search/src/main/java/com/teslahua/shoppingmall/search/Thread/completableFutureTest.java
@@ -16,20 +16,35 @@ public class completableFutureTest {
     public static ExecutorService executorService = Executors.newFixedThreadPool(10);
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         System.out.println("main...start....");
+
         /*
         CompletableFuture<Void> future = CompletableFuture.runAsync(()->{
             System.out.println("当前线程：" + Thread.currentThread().getId());
             int i = 10/2;
             System.out.println("运行结果：" + i);
         },executorService);
-         */
+
         CompletableFuture<Object> future = CompletableFuture.supplyAsync(()->{
             System.out.println("当前线程：" + Thread.currentThread().getId());
             int i = 10/2;
             System.out.println("运行结果：" + i);
             return i;
-        });
+        },executorService);
+        System.out.println("main...stop...."+future.get());
+         */
 
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(()->{
+            System.out.println("当前线程：" + Thread.currentThread().getId());
+            int i = 10 / 0;
+            System.out.println("运行结果：" + i);
+            return i;
+        },executorService).whenComplete((result,exception)->{
+            //虽然能得到异常信息，但是没办法修改返回数据。
+            System.out.println("异步任务成功完成...结果是："+ result +"异常是：" + exception);
+        }).exceptionally(throwable -> {
+            //返回一个默认值
+            return 10;
+        });
         System.out.println("main...stop...."+future.get());
     }
 }
